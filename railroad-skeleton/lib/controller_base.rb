7fc4d41976583edaa6f816908859583721990f4a
Grace Patuwo <grace.patuwo@gmail.com>
2016-09-23 11:10:55 -0700

    created ControllerBase methods to redirect and render response

--- railroad-skeleton/lib/controller_base.rb
+++ railroad-skeleton/lib/controller_base.rb
@@ -6,27 +6,48 @@ require_relative './session'
 class ControllerBase
   attr_reader :req, :res, :params
 
-  # Setup the controller
+  # Setup the controller. Takes in HTTP Request + Response objects
   def initialize(req, res)
+    @req = req
+    @res = res
+    @already_built_response = false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise "you can't render twice :( " if already_built_response?
+    # location is an Response instance method
+    @res['Location'] = url
+    # whereas status isan instance attribute
+    @res.status = 302
+    @already_built_response = true
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise "you can't render twice :( " if already_built_response?
+    @res['Content-Type'] = content_type
+    @res.write(content)
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    controller_name = self.class.to_s.underscore
+    # assuming template naming convention follows this:
+    template_content = File.read("../views/#{controller_name}/#{template_name}.html.erb")
+
+    # Kernel's binding bundles the environmental bindings and makes them available in another context (so in this case, sends to view template)
+    render_content(ERB.new(template_content).result(binding), "text/html")
+
   end
 
   # method exposing a `Session` object
@@ -37,4 +58,3 @@ class ControllerBase
   def invoke_action(name)
   end
 end
-
