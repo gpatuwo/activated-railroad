ff6b788e699228cdc1efa2efec1db277e71badf6
Grace Patuwo <grace.patuwo@gmail.com>
2016-09-23 08:34:24 -0700

    created basic rack middleware

--- railroad-skeleton/bin/p01_basic_server.rb
+++ railroad-skeleton/bin/p01_basic_server.rb
@@ -1,1 +1,17 @@
 require 'rack'
+
+# Rack is a middleware that sits between a web server and a web app framework to make it easier to write frameworks + servers that work with existing software.
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  #  Content-Type tells the browser what the server has given it in response
+  res['Content-Type'] = 'text/html'
+  res.write("Hello world!")
+  res.finish
+end
+
+Rack::Server.start(
+  app: app,
+  Port: 3000
+)
